6c10214ba68ac968c17b78241cc03a6153c930b3
llj <lljdux@123.com>
2023-02-17 14:31:51 +0800

    实现 effect & reactive & 依赖收集 & 触发依赖

---
+++ src/reactivity/effect.ts
@@ -0,0 +1,36 @@
+let activeEffect
+class ReactiveEffect {
+  private _fn: any
+  constructor(fn) {
+    this._fn = fn
+  }
+  run() {
+    activeEffect = this
+    this._fn()
+  }
+}
+const tragetMap = new Map()
+export function track(target, key) {
+  let depsMap = tragetMap.get(target)
+  if (!depsMap) {
+    depsMap = new Map()
+    tragetMap.set(target, depsMap)
+  }
+  let dep = depsMap.get(key)
+  if (!dep) {
+    dep = new Set()
+    depsMap.set(key, dep)
+  }
+  dep.add(activeEffect)
+}
+export function trigger(target, key) {
+  let depsMap = tragetMap.get(target)
+  let dep = depsMap.get(key)
+  for (let d of dep) {
+    d.run()
+  }
+}
+export function effect(fn) {
+  const _effect = new ReactiveEffect(fn)
+  _effect.run()
+}
