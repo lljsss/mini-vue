381db6417d8acc6b896d52c340bd24ea0861502c
llj <lljdux@123.com>
2023-02-21 15:27:25 +0800

    22-实现初始化 element 主流程

--- example/helloworld/app.js
+++ example/helloworld/app.js
@@ -1,7 +1,10 @@
 import { h } from '../../lib/guid-mini-vue.esm.js'
 export const App = {
   render() {
-    return h('div', 'hi,' + this.msg)
+    return h('div', { id: 'root', class: ['red', 'hard'] }, [
+      h('p', { class: 'red' }, 'hi'),
+      h('p', { class: 'blue' }, 'vue'),
+    ])
   },
   setup() {
     return {
--- src/runtime-core/renderer.ts
+++ src/runtime-core/renderer.ts
@@ -1,3 +1,4 @@
+import { isObject } from '../shared/index'
 import { createComponentInstance, setupComponent } from './component'
 
 export function render(vnode, container) {
@@ -9,7 +10,35 @@ function patch(vnode, container) {
   //是 element 那么就应该处理element
   // 去处理组件
   // 判断是不是element
-  processComponent(vnode, container)
+  if (typeof vnode.type === 'string') {
+    processElement(vnode, container)
+  } else if (isObject(vnode.type)) {
+    processComponent(vnode, container)
+  }
+}
+function processElement(vnode: any, container: any) {
+  mountElement(vnode, container)
+}
+function mountElement(vnode: any, container: any) {
+  const el = document.createElement(vnode.type)
+  const { childern } = vnode
+  if (typeof childern === 'string') {
+    el.textContent = childern
+  } else if (Array.isArray(childern)) {
+    mountChildren(vnode, el)
+  }
+  const { props } = vnode
+  for (const key in props) {
+    const val = props[key]
+    el.setAttribute(key, val)
+  }
+
+  container.appendChild(el)
+}
+function mountChildren(vnode, el) {
+  vnode.childern.forEach((v) => {
+    patch(v, el)
+  })
 }
 function processComponent(vnode: any, container: any) {
   mountComponent(vnode, container)
